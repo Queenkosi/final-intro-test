e302995ee4e7153696d50b7320fb46b65c9c0488
Queenkosi <130911572+Queenkosi@users.noreply.github.com>
2024-04-06 00:25:32 -1200

    Add files via upload

---
+++ task.js
@@ -0,0 +1,75 @@
+let buttonOnPage = document.getElementById('btn');
+let bodyElement = document.getElementsByTagName('body');
+
+buttonOnPage.addEventListener('click', clickme);
+let arr =['green', 'red', 'yellow', 'pink', 'blue', 'brown']
+
+function generateColor() {
+    let color = '#';
+}
+for (let i =0; i++; i < arr.length){
+console.log(color);
+}
+
+arr.map((item) => {
+    const button = document.createElement('button');
+    button.style.backgroundColor = (changebgColor);
+    button.addEventListener ('click', () => {
+    });
+}
+);
+colorOptions.appendChild(button);
+function colorWhite() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'white';
+    
+}
+function colorRed() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'red';
+    
+}
+function colorYellow() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'yellow';
+    
+}
+function colorGreen() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'green';
+    
+}
+function colorPink() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'pink';
+    
+}
+function colorBlue() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'blue';
+    
+}
+function colorBrown() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'brown';
+    
+}
+function colorBlack() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'black';
+    
+}
+function colorViolet() {
+   
+    let arr = generateColor();
+    document.body.style.backgroundColor = 'violet';
+    
+}
